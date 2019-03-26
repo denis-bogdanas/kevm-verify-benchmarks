fab14cd8e94ca7a0c3796dcac07b4a0b30e2dd50
Suhabe Bugrara <suhabe@bugrara.com>
2019-03-26 15:40:48 -0700

    remove storage var

--- keccak04/keccak04.sol
+++ keccak04/keccak04.sol
@@ -4,6 +4,6 @@ contract keccak04 {
     uint public nonce;
 
     function getkeccak() public returns(bytes32) {
-        return keccak256(abi.encodePacked(byte(0x19), byte(0x01), nonce));
+        return keccak256(abi.encodePacked(byte(0x19), byte(0x01)));
     }
 }
