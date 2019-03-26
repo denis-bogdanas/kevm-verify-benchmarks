a808af3ec300beddcb0373adc8574771760f57d6
Suhabe Bugrara <suhabe@bugrara.com>
2019-03-26 12:24:44 -0700

    keccak encode constant

--- keccak01/keccak01.sol
+++ keccak01/keccak01.sol
@@ -4,6 +4,6 @@ contract keccak01 {
     uint public nonce;
 
     function getkeccakstorage() public returns(bytes32) {
-        return keccak256(abi.encode(nonce));
+        return keccak256(abi.encode(5));
     }
 }
