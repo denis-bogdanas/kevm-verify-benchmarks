77de3839b0235d47fe5eff282c607e8a987b6614
Suhabe Bugrara <suhabe@bugrara.com>
2019-03-27 07:04:38 -0700

    storage variable encoding

--- encode02/encode02.sol
+++ encode02/encode02.sol
@@ -3,7 +3,7 @@ pragma solidity 0.4.24;
 contract encode02 {
     bytes32 public storage0;
 
-    function get(bytes32 arg1) public returns(bytes) {
-        return abi.encodePacked("\x19\x01", storage0, arg1);
+    function get(bytes32 arg0) public returns(bytes) {
+        return abi.encodePacked("\x19\x01", storage0, arg0);
     }
 }
