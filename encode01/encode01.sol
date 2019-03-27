e81e418b73df1fbde215e9e5d670faa416a327a8
Suhabe Bugrara <suhabe@bugrara.com>
2019-03-27 06:44:51 -0700

    use string to encode primer

--- encode01/encode01.sol
+++ encode01/encode01.sol
@@ -3,6 +3,6 @@ pragma solidity 0.4.24;
 contract encode01 {
 
     function get(bytes32 arg0, bytes32 arg1) public returns(bytes) {
-        return abi.encodePacked(byte(0x19), byte(0x01), arg0, arg1);
+        return abi.encodePacked("\x19\x01", arg0, arg1);
     }
 }
