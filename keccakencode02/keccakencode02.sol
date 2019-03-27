50d844930a5c520583ba13054f2070cc90f602d6
Suhabe Bugrara <suhabe@bugrara.com>
2019-03-27 08:40:01 -0700

    update contract name

--- keccakencode02/keccakencode02.sol
+++ keccakencode02/keccakencode02.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.4.24;
 
-contract keccackencode02 {
+contract keccakencode02 {
 
     function get(bytes32 x) public returns(bytes32) {
         return keccak256(abi.encodePacked(x));
