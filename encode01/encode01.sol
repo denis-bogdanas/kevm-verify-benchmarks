6e446103b5a8a14e63a271b3fc822990acd2821a
Suhabe Bugrara <suhabe@bugrara.com>
2019-03-27 06:42:37 -0700

    encode01 works

--- encode01/encode01.sol
+++ encode01/encode01.sol
@@ -2,7 +2,7 @@ pragma solidity 0.4.24;
 
 contract encode01 {
 
-    function get() public returns(bytes) {
-        return abi.encodePacked(byte(0x19));
+    function get(bytes32 arg0, bytes32 arg1) public returns(bytes) {
+        return abi.encodePacked(byte(0x19), byte(0x01), arg0, arg1);
     }
 }
