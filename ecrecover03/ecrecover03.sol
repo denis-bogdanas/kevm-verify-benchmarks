7d7d741648f16a506ae1b27986e347f5c5466412
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-01 07:27:06 -0700

    initial commit of singlesig10

---
+++ ecrecover03/ecrecover03.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.4.24;
+
+contract ecrecover03 {
+
+    function get(bytes32 hash, uint8 sigV, bytes32 sigR, bytes32 sigS) public returns(uint256) {
+        address recovered = ecrecover(hash, sigV, sigR, sigS);
+        require(recovered > 0);
+        return 5;
+    }
+}
