976d974d6b1515b35c071c7635b68b024f15fc92
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-01 05:15:22 -0700

    ecrecover01

---
+++ ecrecover01/ecrecover01.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.4.24;
+
+contract ecrecover01 {
+
+    function get(bytes32 hash, uint8 sigV, bytes32 sigR, bytes32 sigS) public returns(address) {
+        return ecrecover(hash, sigV, sigR, sigS);
+    }
+}
