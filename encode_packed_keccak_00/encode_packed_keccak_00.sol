28806f8ebf1b0d70e2deaaeaabafc7306b88947b
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-01 08:28:14 -0700

    remove redundant benchmarks

--- encode_packed_keccak_00/encode_packed_keccak_00.sol
+++ encode_packed_keccak_00/encode_packed_keccak_00.sol
@@ -1,9 +1,8 @@
 pragma solidity 0.4.24;
 
-contract keccakencode02 {
+contract encode_packed_keccak_00 {
 
     function get(bytes32 x) public returns(bytes32) {
         return keccak256(abi.encodePacked(x));
     }
 }
-
--- keccakencode00/keccackencode00.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.4.24;
-
-contract keccackencode00 {
-
-    function get(uint x) public returns(bytes32) {
-        return keccak256(abi.encodePacked(x));
-    }
-}
