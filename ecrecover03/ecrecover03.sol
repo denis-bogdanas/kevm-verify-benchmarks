8bc405394ecd57b25f7a803e31fbfd4886c4bad5
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-03 11:43:27 -0700

    singlesig12 passes

--- ecrecover03/ecrecover03.sol
+++ ecrecover03/ecrecover03.sol
@@ -1,10 +1,11 @@
 pragma solidity 0.4.24;
 
 contract ecrecover03 {
+    mapping (address => bool) isOwner;
 
     function get(bytes32 hash, uint8 sigV, bytes32 sigR, bytes32 sigS) public returns(uint256) {
         address recovered = ecrecover(hash, sigV, sigR, sigS);
-        require(recovered > 0);
+        require(recovered > 0 && isOwner[recovered]);
         return 5;
     }
 }
