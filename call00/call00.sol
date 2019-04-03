9bde0fcf89267c9331fbe84c2617d5fd9f77c81d
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-03 07:41:19 -0700

    call00 example; worked when callee bytecode specified

--- call00/call00.sol
+++ call00/call00.sol
@@ -2,9 +2,9 @@ pragma solidity 0.4.24;
 
 contract call00 {
 
-    function execute(address destination, uint gasLimit) public returns(bool) {
+    function execute(address destination) public returns(bool) {
         bool success = false;
-        assembly { success := call(gasLimit, destination, 0, 0, 0, 0, 0) }
+        assembly { success := call(0, destination, 0, 0, 0, 0, 0) }
         return success;
     }
 }
