3e473eeb92402129df7234b60be175fae633be0a
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-03 15:35:53 -0700

    storage mapping

--- storage_mapping_00/storage_mapping_00.sol
+++ storage_mapping_00/storage_mapping_00.sol
@@ -1,9 +1,9 @@
 pragma solidity 0.4.24;
 
 contract storage_mapping_00 {
-    mapping(address => bool) public isOwner;
+    mapping(address => uint256) public isOwnerMap;
 
-    function isOwner(address x) external returns (bool) {
-        return isOwner[x];
+    function isOwner(address x) external returns (uint256) {
+        return isOwnerMap[x];
     }
 }
