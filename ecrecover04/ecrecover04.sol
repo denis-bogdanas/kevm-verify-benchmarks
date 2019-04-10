2783e8fe0073e33f21297368135a6922a8b9381f
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-10 09:22:52 -0700

    ecrecover on hash

--- ecrecover04/ecrecover04.sol
+++ ecrecover04/ecrecover04.sol
@@ -2,7 +2,8 @@ pragma solidity 0.4.24;
 
 contract ecrecover04 {
 
-    function get(bytes32 hash, uint8 sigV, bytes32 sigR, bytes32 sigS) public returns(uint256) {
+    function get(bytes32 arg0, bytes32 arg1, uint8 sigV, bytes32 sigR, bytes32 sigS) public returns(uint256) {
+        bytes32 hash = keccak256(abi.encodePacked("\x19\x01", arg0, arg1));
         address recovered = ecrecover(hash, sigV, sigR, sigS);
         require(recovered > 0);
         return 5;
