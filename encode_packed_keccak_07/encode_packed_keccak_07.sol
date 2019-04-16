fdbff7313b9c83d0bd97a0e2437b65d72e1f185a
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-15 20:39:58 -0700

    fails

--- encode_packed_keccak_07/encode_packed_keccak_07.sol
+++ encode_packed_keccak_07/encode_packed_keccak_07.sol
@@ -4,10 +4,10 @@ contract encode_packed_keccak_07 {
 
     bytes32 DOMAIN_SEPARATOR = 0xdeadbeef;
 
-    function execute(bytes data, uint gasLimit) external returns(bytes32) {
+    function execute(uint gasLimit) external returns(bytes32) {
 
         bytes32 txInputHash = keccak256(abi.encode(gasLimit));
-        bytes32 txTotalHash = keccak256(abi.encodePacked(byte(25), byte(1), txInputHash));
+        bytes32 txTotalHash = keccak256(abi.encodePacked(byte(25), byte(1), gasLimit));
 
         return txTotalHash;
     }
