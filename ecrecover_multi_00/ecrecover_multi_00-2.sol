03163727a3a5ce7f9334d05c496eabd3850f135b
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-16 09:04:14 -0700

    static array spec'd out

--- ecrecover_multi_00/ecrecover_multi_00-2.sol
+++ ecrecover_multi_00/ecrecover_multi_00-2.sol
@@ -5,10 +5,10 @@ contract ecrecover_multi_00 {
 
     function get(bytes32 hash, uint8[3] sigV, bytes32[3] sigR, bytes32[3] sigS) public returns(uint256) {
 
-        /*
         for (uint i = 0; i < THRESHOLD; i++) {
             address recovered = ecrecover(hash, sigV[i], sigR[i], sigS[i]);
-        }*/
+            require(recovered > 0);
+        }
 
         return 5;
     }
