e08e5941012db11896a59ec0faa09563049ec92f
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-16 04:55:01 -0700

    isolate simplication issue

--- encode_packed_keccak_07/encode_packed_keccak_07.sol
+++ encode_packed_keccak_07/encode_packed_keccak_07.sol
@@ -2,9 +2,9 @@ pragma solidity 0.4.24;
 
 contract encode_packed_keccak_07 {
 
-    function execute(uint value, uint gasLimit) external returns(bytes32) {
+    function execute(bytes data, uint gasLimit) external returns(bytes32) {
 
-        bytes32 txInputHash = keccak256(abi.encode(value, gasLimit));
+        bytes32 txInputHash = keccak256(abi.encode(keccak256(data), gasLimit));
 
         bytes32 txTotalHash = keccak256(
                 abi.encodePacked(byte(25), byte(1), txInputHash));
