a5daf3d20dd6c3759ef09096465ee867538a9374
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-15 20:34:03 -0700

    break down multisig encoding

--- encode_packed_keccak_07/encode_packed_keccak_07.sol
+++ encode_packed_keccak_07/encode_packed_keccak_07.sol
@@ -6,8 +6,8 @@ contract encode_packed_keccak_07 {
 
     function execute(bytes data, uint gasLimit) external returns(bytes32) {
 
-        bytes32 txInputHash = keccak256(abi.encode(keccak256(data), gasLimit));
-        bytes32 txTotalHash = keccak256(abi.encodePacked(byte(25), byte(1), DOMAIN_SEPARATOR, txInputHash));
+        bytes32 txInputHash = keccak256(abi.encode(gasLimit));
+        bytes32 txTotalHash = keccak256(abi.encodePacked(byte(25), byte(1), txInputHash));
 
         return txTotalHash;
     }
