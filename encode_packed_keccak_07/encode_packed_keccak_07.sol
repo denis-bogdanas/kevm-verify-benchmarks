193af13c4daed7fb1563dfcceaef0df5b34309d9
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-15 20:22:32 -0700

    fix buggy spec

--- encode_packed_keccak_07/encode_packed_keccak_07.sol
+++ encode_packed_keccak_07/encode_packed_keccak_07.sol
@@ -6,7 +6,7 @@ contract encode_packed_keccak_07 {
 
     function execute(bytes data, uint gasLimit) external returns(bytes32) {
 
-        bytes32 txInputHash = keccak256(abi.encode(gasLimit, keccak256(data)));
+        bytes32 txInputHash = keccak256(abi.encode(keccak256(data), gasLimit));
         bytes32 txTotalHash = keccak256(abi.encodePacked(byte(25), byte(1), DOMAIN_SEPARATOR, txInputHash));
 
         return txTotalHash;
