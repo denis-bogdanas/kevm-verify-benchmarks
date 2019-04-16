ad3d778128016cefebac8deccf73e29a58d10dae
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-15 20:46:36 -0700

    passes

--- encode_packed_keccak_07/encode_packed_keccak_07.sol
+++ encode_packed_keccak_07/encode_packed_keccak_07.sol
@@ -2,14 +2,12 @@ pragma solidity 0.4.24;
 
 contract encode_packed_keccak_07 {
 
-    bytes32 DOMAIN_SEPARATOR = 0xdeadbeef;
-
     function execute(uint gasLimit) external returns(bytes32) {
 
         bytes32 txInputHash = keccak256(abi.encode(gasLimit));
 
         bytes32 txTotalHash = keccak256(
-                abi.encodePacked(byte(25), byte(1), gasLimit));
+                abi.encodePacked(byte(25), byte(1), txInputHash));
 
         return txTotalHash;
     }
