8e9e19e19164d5618f6ccffc5fc0be2ca2dcd540
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-16 09:15:58 -0700

    initial commit

---
+++ ecrecover_multi_02/ecrecover_multi_02.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.4.24;
+
+contract ecrecover_multi_02 {
+
+    uint constant THRESHOLD = 3;
+
+    function get(bytes32 hash, uint8[3] sigV, bytes32[3] sigR, bytes32[3] sigS) public returns(uint256) {
+
+        for (uint i = 0; i < THRESHOLD; i++) {
+            address recovered = ecrecover(hash, sigV[i], sigR[i], sigS[i]);
+            require(recovered > 0);
+        }
+
+        return 5;
+    }
+}
