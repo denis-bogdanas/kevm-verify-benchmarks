731bd4c6e3f355ebbcdc16f9d133db71bb5ec620
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-16 08:54:27 -0700

    ecrecover over arrays

--- ecrecover_multi_00/ecrecover_multi_00.sol
+++ ecrecover_multi_00/ecrecover_multi_00.sol
@@ -1,10 +1,15 @@
 pragma solidity 0.4.24;
 
 contract ecrecover_multi_00 {
+    uint256 constant THRESHOLD = 3;
 
-    function get(bytes32 hash, uint8 sigV, bytes32 sigR, bytes32 sigS) public returns(uint256) {
-        address recovered = ecrecover(hash, sigV, sigR, sigS);
-        require(recovered > 0);
+    function get(bytes32 hash, uint8[3] sigV, bytes32[3] sigR, bytes32[3] sigS) public returns(uint256) {
+
+        for (uint i = 0; i < THRESHOLD; i++) {
+            address recovered = ecrecover(hash, sigV[i], sigR[i], sigS[i]);
+            require(recovered > 0);
+        }
+
         return 5;
     }
 }
