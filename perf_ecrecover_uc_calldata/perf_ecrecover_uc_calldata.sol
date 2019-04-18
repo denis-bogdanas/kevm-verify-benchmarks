51d114b53d55b695fb32da584e77918c13224879
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-18 09:40:38 -0700

    spec passes

--- perf_ecrecover_uc_calldata/perf_ecrecover_uc_calldata.sol
+++ perf_ecrecover_uc_calldata/perf_ecrecover_uc_calldata.sol
@@ -2,8 +2,8 @@ pragma solidity 0.5.0;
 
 contract perf_ecrecover_uc_calldata {
 
-    function get(bytes32 hash, uint8 sigV, bytes32 sigR, bytes32 sigS) public returns(address) {
-        return ecrecover(hash, sigV, sigR, sigS);
+    function get(bytes32 hash, uint8 sigV, bytes32 sigR, bytes32 sigS) public {
+        require(ecrecover(hash, sigV, sigR, sigS) > address(0));
     }
 }
 
