e3e300fd50196f26f7102b7563a7c6faf98fcadd
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-18 16:11:23 -0700

    perf still exists

--- SimpleMultiSigK3_00_perf_fix_01/SimpleMultiSigK3_00.sol
+++ SimpleMultiSigK3_00_perf_fix_01/SimpleMultiSigK3_00.sol
@@ -2,13 +2,15 @@ pragma solidity 0.5.0;
 
 contract SimpleMultiSigK3_00 {
 
-    uint256 constant THRESHOLD = 3;
+    uint256 constant THRESHOLD = 2;
 
-    function execute(uint8[3] memory sigV, bytes32[3] memory sigR, bytes32[3] memory sigS, bytes memory data)
+    function execute(uint8[2] memory sigV, bytes32[2] memory sigR, bytes32[2] memory sigS, bytes memory data)
         public returns(uint256) {
 
+        bytes32 hash = 0x0;
+
         for (uint i = 0; i < THRESHOLD; i++) {
-            address recovered = ecrecover("0x19", sigV[i], sigR[i], sigS[i]);
+            address recovered = ecrecover(hash, sigV[i], sigR[i], sigS[i]);
             require(recovered > address(0));
         }
 
