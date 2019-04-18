44e356d9767d9faa82b40f1ce875a53a893e6467
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-18 13:59:43 -0700

    removing total hash still has the bug

--- SimpleMultiSigK3_00_perf_fix_01/SimpleMultiSigK3_00.sol
+++ SimpleMultiSigK3_00_perf_fix_01/SimpleMultiSigK3_00.sol
@@ -4,12 +4,12 @@ contract SimpleMultiSigK3_00 {
 
     uint256 constant THRESHOLD = 3;
 
-    function execute(uint8[3] memory sigV, bytes32[3] memory sigR, bytes32[3] memory sigS, bytes memory data, bytes32 totalHash)
+    function execute(uint8[3] memory sigV, bytes32[3] memory sigR, bytes32[3] memory sigS, bytes memory data)
         public returns(uint256) {
 
         address lastAdd = address(0);
         for (uint i = 0; i < THRESHOLD; i++) {
-            address recovered = ecrecover(totalHash, sigV[i], sigR[i], sigS[i]);
+            address recovered = ecrecover("0x19", sigV[i], sigR[i], sigS[i]);
             require(recovered > lastAdd);
             lastAdd = recovered;
         }
