a77ea9c955d252e66b3887647ef2cd0d37efd632
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-18 10:10:58 -0700

    current state

--- perf_ecrecover_uc_calldata/perf_ecrecover_uc_calldata.sol
+++ perf_ecrecover_uc_calldata/perf_ecrecover_uc_calldata.sol
@@ -3,7 +3,11 @@ pragma solidity 0.5.0;
 contract perf_ecrecover_uc_calldata {
 
     function get(bytes32 hash, uint8 sigV, bytes32 sigR, bytes32 sigS) public {
-        require(ecrecover(hash, sigV, sigR, sigS) > address(0));
+        address last = address(0);
+        for (uint i = 0; i < 3; i++) {
+            address recovered = ecrecover(hash, sigV, sigR, sigS);
+            require(recovered > last);
+        }
     }
 }
 
