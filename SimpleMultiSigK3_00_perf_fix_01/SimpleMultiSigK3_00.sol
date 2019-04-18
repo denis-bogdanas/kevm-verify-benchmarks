9057504a4a41bcecdd1954b7628393c7fd321b4a
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-18 13:26:27 -0700

    removing isOwner doesn't fix perf

--- SimpleMultiSigK3_00_perf_fix_01/SimpleMultiSigK3_00.sol
+++ SimpleMultiSigK3_00_perf_fix_01/SimpleMultiSigK3_00.sol
@@ -3,7 +3,6 @@ pragma solidity 0.5.0;
 contract SimpleMultiSigK3_00 {
 
     uint256 constant THRESHOLD = 3;
-    mapping (address => bool) isOwner;
 
     function execute(   uint8[3] memory sigV, bytes32[3] memory sigR, bytes32[3] memory sigS,
                         address destination, uint value, bytes memory data, address executor, uint gasLimit, bytes32 totalHash)
@@ -12,7 +11,7 @@ contract SimpleMultiSigK3_00 {
         address lastAdd = address(0);
         for (uint i = 0; i < THRESHOLD; i++) {
             address recovered = ecrecover(totalHash, sigV[i], sigR[i], sigS[i]);
-            require(recovered > lastAdd && isOwner[recovered]);
+            require(recovered > lastAdd);
             lastAdd = recovered;
         }
 
