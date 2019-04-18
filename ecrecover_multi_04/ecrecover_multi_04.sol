3f69ec27186af06862dd9d4ac776be7b02725a33
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-18 08:56:19 -0700

    benchmark for isOwner check

---
+++ ecrecover_multi_04/ecrecover_multi_04.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.5.0;
+
+contract ecrecover_multi_04 {
+
+    uint constant THRESHOLD = 3;
+    mapping(address => bool) isOwner;
+
+    function get(bytes32 hash, uint8[3] memory sigV, bytes32[3] memory sigR, bytes32[3] memory sigS) public returns(uint256) {
+
+        address lastAdd = address(0);
+        for (uint i = 0; i < THRESHOLD; i++) {
+            address recovered = ecrecover(hash, sigV[i], sigR[i], sigS[i]);
+            require(recovered > lastAdd && isOwner[recovered]);
+            lastAdd = recovered;
+        }
+
+        return 5;
+    }
+}
