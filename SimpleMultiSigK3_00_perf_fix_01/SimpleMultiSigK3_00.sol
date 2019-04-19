a151a1ef4cf71d2b3d8cbf315dd64268d09dd84f
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-18 18:49:40 -0700

    chokes

--- SimpleMultiSigK3_00_perf_fix_01/SimpleMultiSigK3_00.sol
+++ SimpleMultiSigK3_00_perf_fix_01/SimpleMultiSigK3_00.sol
@@ -2,13 +2,13 @@ pragma solidity 0.5.0;
 
 contract SimpleMultiSigK3_00 {
 
-    function execute(uint8 sigV, bytes32 sigR, bytes32 sigS, bytes memory data)
+    function execute(uint8[2] memory sigV, bytes32[2] memory sigR, bytes32[2] memory sigS, bytes memory data)
         public returns(uint256) {
 
         bytes32 hash = 0x0;
 
-        for (int i = 0; i < 1; i++) {
-            address recovered = ecrecover(hash, sigV, sigR, sigS);
+        for (uint i = 0; i < 2; i++) {
+            address recovered = ecrecover(hash, 0, 0, 0);
             require(recovered > address(0));
         }
 
