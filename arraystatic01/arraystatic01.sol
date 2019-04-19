21bb4bb99b12533fc1a3007ae23f6e44b6270b7a
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-19 07:03:07 -0700

    fix spec

--- arraystatic01/arraystatic01.sol
+++ arraystatic01/arraystatic01.sol
@@ -1,8 +1,8 @@
-pragma solidity 0.4.24;
+pragma solidity 0.5.0;
 
 contract arraystatic01 {
 
-    function get5(uint[3] x) pure external returns (uint) {
+    function get5(uint[3] memory x) public returns (uint) {
         return x[1];
     }
 }
