0c3721f12cc0830fd8f1f621dac638e4a0e8e29d
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-23 08:33:09 -0700

    passes

--- ecrecover_multi_07/ecrecover_multi_07.sol
+++ ecrecover_multi_07/ecrecover_multi_07.sol
@@ -5,11 +5,11 @@ contract ecrecover_multi_07 {
     uint constant THRESHOLD = 3;
     mapping(address => bool) isOwner;
 
-    function get(bytes32 hash, uint8[3] memory sigV, bytes32[3] memory sigR, bytes32[3] memory sigS, bytes memory data) public returns(uint256) {
+    function get(uint8[3] memory sigV, bytes32[3] memory sigR, bytes32[3] memory sigS, bytes memory data, bytes32 totalHash) public returns(uint256) {
 
         address lastAdd = address(0);
         for (uint i = 0; i < THRESHOLD; i++) {
-            address recovered = ecrecover(hash, sigV[i], sigR[i], sigS[i]);
+            address recovered = ecrecover(totalHash, sigV[i], sigR[i], sigS[i]);
             require(recovered > lastAdd && isOwner[recovered]);
             lastAdd = recovered;
         }
