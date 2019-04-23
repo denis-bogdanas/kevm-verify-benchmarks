eb031d921a5e9e72ba9ffe924642fd4fcd30be97
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-23 14:52:33 -0700

    initial C version

---
+++ ecrecover_multi_02_C/ecrecover_multi_02_C.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.5.0;
+
+contract ecrecover_multi_02_C {
+
+    uint constant THRESHOLD = 3;
+
+    function get(bytes32 hash, uint8[3] memory sigV, bytes32[3] memory sigR, bytes32[3] memory sigS) public returns(uint256) {
+
+        for (uint i = 0; i < THRESHOLD; i++) {
+            address recovered = ecrecover(hash, sigV[i], sigR[i], sigS[i]);
+            require(recovered > address(0));
+        }
+
+        return 5;
+    }
+}
