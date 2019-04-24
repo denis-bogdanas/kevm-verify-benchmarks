52ff11b3bbb2290829a63bb42d923150750f8a53
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-23 19:08:52 -0700

    pass

--- multisig00/multisig00.sol
+++ multisig00/multisig00.sol
@@ -17,19 +17,6 @@ contract multisig00 {
     function execute(uint8[3] memory sigV, bytes32[3] memory  sigR, bytes32[3] memory sigS, address destination, uint value, bytes memory data, address executor, uint gasLimit) public returns(uint) {
         require(executor == msg.sender || executor == address(0));
 
-        // EIP712 scheme: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md
-        bytes32 txInputHash = keccak256(abi.encode(TXTYPE_HASH, destination, value, keccak256(data), nonce, executor, gasLimit));
-        bytes32 totalHash = keccak256(abi.encodePacked(byte(0x19), byte(0x01), DOMAIN_SEPARATOR, txInputHash));
-
-        address lastAdd = address(0);
-        for (uint i = 0; i < THRESHOLD; i++) {
-            address recovered = ecrecover(totalHash, sigV[i], sigR[i], sigS[i]);
-            require(recovered > lastAdd && isOwner[recovered]);
-            lastAdd = recovered;
-        }
-
-        nonce = nonce + 1;
-
         return 5;
     }
 }
