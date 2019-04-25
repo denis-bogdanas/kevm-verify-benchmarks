e368aa087ba06e67c727f295b97979ffeee8da7a
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-25 07:34:26 -0700

    minimize changes to build files

---
+++ ecrecover_multi_02_F_ex_dest_exec_value_gaslimit/ecrecover_multi_02_ex_dest_exec_value_gaslimit.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.5.0;
+
+//get-failure-1 does not terminate
+contract ecrecover_multi_02_ex_dest_exec_value_gaslimit {
+
+    uint constant THRESHOLD = 3;
+
+    function get(uint8[3] memory sigV, bytes32[3] memory sigR, bytes32[3] memory sigS, bytes memory data, bytes32 hash) public returns(uint256) {
+
+        for (uint i = 0; i < THRESHOLD; i++) {
+            address recovered = ecrecover(hash, sigV[i], sigR[i], sigS[i]);
+            require(recovered > address(0));
+        }
+
+        return 5;
+    }
+}
