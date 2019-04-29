62a505a8672592ee10cd92f977bfc00df85c67c3
Suhabe Bugrara <suhabe@bugrara.com>
2019-04-29 12:51:46 -0700

    passing

---
+++ ecrecover_rv_ver2/ecrecover_rv_ver2.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.5.0;
+
+contract ecrecover_rv_ver2 {
+
+    uint constant THRESHOLD = 2;
+
+    function get(bytes memory data, bytes32 hash) public returns(uint256) {
+        address recovered = ecrecover(hash, uint8(uint(1)), bytes32(uint(1)), bytes32(uint(1)));
+        require(recovered > address(0));
+
+        recovered = ecrecover(hash, uint8(uint(1)), bytes32(uint(1)), bytes32(uint(1)));
+        require(recovered > address(0));
+
+        return 5;
+    }
+}
