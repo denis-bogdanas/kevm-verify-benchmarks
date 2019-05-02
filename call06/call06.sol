434bcb79a4d3f5d45e9a06a004dcccdb039a36d6
Suhabe Bugrara <suhabe@bugrara.com>
2019-05-02 12:04:51 -0700

    passes

--- call06/call06.sol
+++ call06/call06.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.0;
 
 contract call06 {
 
-    function execute(address destination) public returns(bool) {
+    function execute(bool condition, address destination) public returns(bool) {
         bool success = false;
         assembly { success := call(gas, destination, 0, 0, 0, 0, 0) }
         return success;
