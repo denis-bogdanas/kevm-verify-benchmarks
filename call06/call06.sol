b24cd999dbb34ba2c7c5e6d73a99c7be346fd381
Suhabe Bugrara <suhabe@bugrara.com>
2019-05-02 13:04:50 -0700

    initial

---
+++ call06/call06.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.5.0;
+
+contract call05 {
+
+    function execute(bool condition, address destination, uint value, bytes memory data, uint gasLimit) public returns(bool) {
+        require(condition);
+
+        bool success = false;
+        assembly { success := call(gasLimit, destination, value, add(data, 0x20), mload(data), 0, 0) }
+        return success;
+    }
+}
