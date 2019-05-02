3bf09c6a99165ac898406d6ba0aaab4b4d048ef0
Suhabe Bugrara <suhabe@bugrara.com>
2019-05-02 11:22:02 -0700

    passes

--- call05/call05.sol
+++ call05/call05.sol
@@ -4,7 +4,7 @@ contract call05 {
 
     function execute(address destination, uint value, bytes memory data, uint gasLimit) public returns(bool) {
         bool success = false;
-        assembly { success := call(gas, destination, 0, 0, 0, 0, 0) }
+        assembly { success := call(gasLimit, destination, value, add(data, 0x20), mload(data), 0, 0) }
         return success;
     }
 }
