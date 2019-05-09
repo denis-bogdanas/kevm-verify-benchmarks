da307f77868395b3f195c82a1dade94c68e0dd13
Suhabe Bugrara <suhabe@bugrara.com>
2019-05-09 09:16:22 -0700

    call rules

--- call20/call20.sol
+++ call20/call20.sol
@@ -1,12 +1,13 @@
 pragma solidity 0.5.0;
 
-contract call06 {
+contract call20 {
 
-    function execute(bool condition, address destination, uint value, bytes memory data, uint gasLimit) public returns(bool) {
+    function execute(bool condition, address destination, uint value, bytes memory data, uint gasLimit) public {
         require(condition);
 
         bool success = false;
         assembly { success := call(gasLimit, destination, value, add(data, 0x20), mload(data), 0, 0) }
-        return success;
+        require(success);
     }
 }
+
