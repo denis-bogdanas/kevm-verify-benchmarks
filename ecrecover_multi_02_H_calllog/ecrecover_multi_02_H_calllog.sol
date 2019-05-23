f3fbc57d76e14141cd5fd75ef7976f892dca807e
Suhabe Bugrara <suhabe@bugrara.com>
2019-05-23 09:57:53 -0700

    calllog

--- ecrecover_multi_02_H_calllog/ecrecover_multi_02_H_calllog.sol
+++ ecrecover_multi_02_H_calllog/ecrecover_multi_02_H_calllog.sol
@@ -5,13 +5,15 @@ contract ecrecover_multi_02_H_calllog {
 
     uint constant THRESHOLD = 2;
 
-    function get(bytes memory data, bytes32 hash) public returns(uint256) {
+    function get(bool condition, uint gasLimit, uint value, bytes memory data, address destination, bytes32 hash) public {
 
         for (uint i = 0; i < THRESHOLD; i++) {
             address recovered = ecrecover(hash, uint8(i), bytes32(i), bytes32(i));
             require(recovered > address(0));
         }
 
-        return 5;
+        bool success = false;
+        assembly { success := call(gasLimit, destination, value, add(data, 0x20), mload(data), 0, 0) }
+        require(success);
     }
 }
