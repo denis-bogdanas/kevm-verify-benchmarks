933ad138fe6b86a18f83634dfef2539f2b4b2e54
Suhabe Bugrara <suhabe@bugrara.com>
2019-05-23 08:08:12 -0700

    simple benchmark that passes when CALL opcode not specified

---
+++ call_log_02/call_log_02.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.5.0;
+
+contract call_log_02 {
+
+    function execute(bool condition, uint gasLimit, uint value, bytes memory data, address destination) public returns(bool) {
+        require(condition);
+
+        bool success = false;
+        assembly { success := call(gasLimit, destination, value, add(data, 0x20), mload(data), 0, 0) }
+        require(success);
+    }
+}
