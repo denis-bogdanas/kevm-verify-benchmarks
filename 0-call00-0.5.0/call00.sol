abb7d93d11dfb33a571db792c1dd54b44bdc6e02
Suhabe Bugrara <suhabe@bugrara.com>
2019-06-03 13:21:39 -0700

    remove return type

--- 0-call00-0.5.0/call00.sol
+++ 0-call00-0.5.0/call00.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.0;
 
 contract call00 {
 
-    function execute(bool condition, uint gasLimit, uint value, bytes memory data, address destination) public returns(bool) {
+    function execute(bool condition, uint gasLimit, uint value, bytes memory data, address destination) public {
         require(condition);
 
         bool success = false;
